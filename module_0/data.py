886c96c0fee07caac404c9a1de5f15f45f227a73
darthmorgot <dartmorgot@yandex.ru>
2024-09-27 23:48:31 +1000

    Занятие "Базовые структуры данных"

---
+++ module_0/data.py
@@ -0,0 +1,27 @@
+# number
+print(5)
+print(type(5))  # int
+print(5 + 5)
+print(5 / 5)  # 1.0
+print(5 // 5)  # 1
+print(5 % 3)
+print(5 ** 3)
+print(type(2.0))  # float
+# float + int = float
+
+# string
+print('hello world')
+print(type('test'))  # str
+print('test \'str\'')
+print('one ' + 'two')
+# print('1' + 3) # error
+
+# boolean
+print(True, False)  # bool
+print(4 < 9)
+print(5 != 5 and 6 <= 5)
+print(5 != 5 or 6 <= 5)
+
+# data convert
+print(int('45'))
+# print(int('root'))
