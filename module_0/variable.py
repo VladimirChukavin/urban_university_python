455182dbcce2ba15be6ca0cdbd544dd875e380b2
darthmorgot <dartmorgot@yandex.ru>
2024-09-28 21:58:19 +1000

    Занятие и практическое задание "Переменные"

---
+++ module_0/variable.py
@@ -0,0 +1,13 @@
+name = "Vladimir"
+print(name)
+name = "Jane"
+print(name)
+date_birth = "May 30"
+print(date_birth)
+
+# homework
+number_of_completed_homework = 12
+number_of_hours = 1.5
+course_name = "Python"
+time_for_one_task = 1.5 / 12
+print(f"Курс: {course_name}, всего задач: {number_of_completed_homework}, затрачено часов: {number_of_hours}, среднее время выполнения {time_for_one_task}.")
