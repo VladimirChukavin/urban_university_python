1018e83e5dc345e24182125ffdf8b96e7dc7ad90
darthmorgot <dartmorgot@yandex.ru>
2024-09-28 21:59:06 +1000

    Занятие и практическое задание "Строки и индексация строк"

---
+++ module_0/strings.py
@@ -0,0 +1,21 @@
+name_1 = "John"
+name_2 = 'Laneson'
+print(name_1 + " " + name_2)
+print("twist" * 4)
+print(name_1[0])
+print(name_1[-1])
+print(name_1[0:3])
+print(name_1[:3])
+print(name_2[0::2])
+print(name_2[2:])
+print(name_2[::])
+print(name_2[::-1])
+
+# homework
+example = "university"
+print(example[0])
+print(example[-1])
+# print(example[5:])
+print(example[len(example)//2:])
+print(example[::-1])
+print(example[1::2])
