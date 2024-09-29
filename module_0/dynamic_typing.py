6bb6fe066e52e1a9b38979f34442ac358144f874
darthmorgot <dartmorgot@yandex.ru>
2024-09-29 20:01:33 +1000

    Занятие и практическое задание "Динамическая типизация"

---
+++ module_0/dynamic_typing.py
@@ -0,0 +1,19 @@
+name = "Urban"
+print(id(name))
+print(name, type(name))
+name = 5
+print(name, type(name))
+name = True
+print(name, type(name))
+name = [1, 2]
+print(name, type(name))
+
+# homework (module_1_3.py)
+name = "Vladimir"
+print("Name:", name)
+age = 48
+print("Age:", age)
+age = age + 1
+print("New Age:", age)
+is_student = True
+print("Is Student:", is_student)
