af9a8b8a41329c45cf45c7c9f36313a745ab5b7f
darthmorgot <dartmorgot@yandex.ru>
2024-09-30 22:24:35 +1000

    Занятие и практическое задание "Организация программ и методы строк"

---
+++ module_0/string_methods.py
@@ -0,0 +1,20 @@
+# name = input("Enter your name: ")
+# current_year = 2024
+# date_of_birth = int(input("Enter your date of birth: "))
+# age = current_year - date_of_birth
+# print("Welcome,", name)
+# print("In current year", age)
+
+# print('alkfj a lsfk jal as'.upper())
+# print('LHIGB KJL'.lower())
+# print('test test test'.replace('e', 'o'))
+# print('test test test'.replace(' ', '_'))
+
+# homework module_1_4.py
+my_string = input("Enter a string: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
