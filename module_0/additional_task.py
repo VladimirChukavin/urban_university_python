58c05cf23faae15ef1d40a40bcc1dba3a8101347
VladimirChukavin <dartmorgot@yandex.ru>
2024-10-02 16:09:38 +1000

    Дополнительное практическое задание по модулю

---
+++ module_0/additional_task.py
@@ -0,0 +1,5 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+# {'Aaron': 4.0, 'Bilbo': 2.25, 'Johhny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
+print(dict(zip(sorted(students), [sum(n)/len(n) for n in grades])))
