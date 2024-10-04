ed02cbdbfe8413a457a06ceda84fbac20456f347
VladimirChukavin <dartmorgot@yandex.ru>
2024-10-04 14:26:42 +1000

    Занятие и практическое задание "Цели и задачи. Поток выполнения программы"

---
+++ module_2/program_execution_flow.py
@@ -0,0 +1,15 @@
+# Поток выполнения программы. Как интерпретатор показывает переменные
+
+# from time import sleep
+
+# a = 5
+# print(a)
+# sleep(2)
+# print('timeout')
+
+# homework
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
