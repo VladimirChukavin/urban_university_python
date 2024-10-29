f76b6cfd30cf0a29e3e801c2b9b9089e64777cb7
VladimirChukavin <dartmorgot@yandex.ru>
2024-10-29 16:36:48 +1000

    Занятие "Пространства имен часть II и области видимости"

--- module_4_5/namespace.py
+++ module_4_5/namespace.py
@@ -1,4 +1,5 @@
 # Пространство имен
+# Пространства имен часть II и области видимости
 # import this
 import math
 
@@ -18,11 +19,32 @@ def square(x):
     print(locals())
     return a
 
+
 def square_alt():
     return a ** 2
+
 
 b = square(2)
 print(a)
 print(b)
 print(globals())
 print(square_alt())
+
+
+def square_one(x):
+    d = x ** 2
+
+    def even(x):
+        # d = x * 2
+        nonlocal d
+        d = x / 2
+        if x % 2 == 0:
+            print('Even')
+        else:
+            print('Odd')
+
+    even(x)
+    return d
+
+print(square_one(4))
+print(square_one(3))
