f52751f433faf27c6b192419d8afc4869aca3bbc
VladimirChukavin <dartmorgot@yandex.ru>
2024-12-27 17:02:07 +1000

    Занятие "Обзор сторонних библиотек. 1.1"

---
+++ module_11/overview_libraries_1.py
@@ -0,0 +1,12 @@
+# Обзор сторонних библиотек. 1.1
+
+# pip search <key_word> - найти все пакеты на PyPI с этим словом
+# pip install <package_name> - найти и установить последнюю версию пакета
+# pip install <package_name>==1.2.3 - найти и установить последнюю версию пакета 1.2.3
+# pip install --upgrade <package_name> - обновить версию установленного пакета до последней
+# pip install --upgrade <package_name>==1.2.3 - обновить версию установленного пакета до указанной
+# pip uninstall <package_name> - удалить пакет
+# pip freeze - выдать список установленных пакетов с версиями
+# pip list - тоже, что и freeze только в виде таблицы
+# pip freeze > requirements.txt - создать файл, в котором указаны все пакеты с версиями
+# pip install -r requirements.txt - установить все пакеты указанных версий из файла (рекурсивно)
